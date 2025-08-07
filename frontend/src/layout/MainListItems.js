6438cc03df9a32c2f643fe17b8a10ad662ca2bd3
glecson007 <ascom@camponovo.ro.gov.br>
2025-08-07 17:14:24 -0400

    Update MainListItems.js

--- frontend/src/layout/MainListItems.js
+++ frontend/src/layout/MainListItems.js
@@ -462,17 +462,19 @@ const MainListItems = (props) => {
               icon={<SettingsOutlinedIcon />}
             />
             
-              <Divider />
-              <Typography style={{ fontSize: "12px", padding: "10px", textAlign: "right", fontWeight: "bold" }}>
-                {`${gitinfo.tagName || gitinfo.branchName + " " + gitinfo.commitHash }`} 
-                &nbsp;/&nbsp;
-                {`${gitinfo.buildTimestamp }`}
-              </Typography>
-            
-          </>
-        )}
-      />
-      <Divider />
+               <Divider />
+      <Typography style={{ 
+        fontSize: "12px", 
+        padding: "10px", 
+        textAlign: "right", 
+        fontWeight: "bold" 
+      }}>
+        Versão:6.5.0 - CRM - RO © 2025
+      </Typography>
+    </>
+  )}
+/>
+<Divider />
     </div>
   );
 };
